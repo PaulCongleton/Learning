bd8098fded76c64d893edefe6bac1c08c2159dda
Paul C <47114776+PaulCongleton@users.noreply.github.com>
2019-01-31 17:04:24 -0500

    Add files via upload

---
+++ visit_my_docker_hub.py
@@ -0,0 +1,5 @@
+#spent the afternoon playint with docker containers and images.  I havent pushed much to my docker hub page but heres what I did today
+#
+#https://hub.docker.com/r/dustyfj/newonboarding
+
+print ('time for a beer')
